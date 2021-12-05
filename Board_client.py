6edc8a275b924173c51f19abb5bba5cb34a7d19e
Aik Khachatrian <aik.khachatrian@gmail.com>
2021-12-05 19:29:35 +0200

    Add configurable IP & Port

--- Board_client.py
+++ Board_client.py
@@ -4,6 +4,8 @@ import time
 import random
 from Snake import Snake
 import socket, threading
+from tkinter import *  
+from tkinter import messagebox
 
 class Board:
 
@@ -13,8 +15,42 @@ class Board:
     red = (237, 41, 57)
     blood_red = (138, 3, 3)
     
+    server_address = None
+    server_port = None
+    
+    window = Tk()
+    txt_ip = Entry(window,width=15) 
+    txt_port = Entry(window,width=15) 
+    lbl_ip = Label(window, text=('Imput server IP'))
+    lbl_port = Label(window, text=('Imput server port'))
+    btn = None
+    
+    
     def __init__(self):
         self.snake = Snake((12, 12))
+        
+    def setWindow(self):
+        self.window.title("Window")  
+        self.window.geometry('300x80')
+        self.window.eval('tk::PlaceWindow . center')
+        self.window.resizable(False, False) 
+        
+        self.txt_ip.grid(column=1, row=0) 
+        self.txt_port.grid(column=1, row=1) 
+        self.txt_ip.focus()
+        self.lbl_ip.grid(column=0, row=0)
+        self.lbl_port.grid(column=0, row=1)
+        self.btn = Button(self.window, text="Enter", command= lambda: self.getAddress((self.txt_ip.get(), int(self.txt_port.get()))))
+        self.btn.grid(column=2, row=2)
+        self.window.mainloop()
+        
+        
+    def getAddress(self, address_and_port: tuple):
+        print(address_and_port)
+        self.server_address = address_and_port[0]
+        self.server_port = address_and_port[1]
+        
+        self.window.destroy()
         
     def handle_messages(self, connection: socket.socket):
         
@@ -88,94 +124,98 @@ class Board:
 
     def main(self):
         
-        SERVER_ADDRESS = '127.0.0.1'
-        SERVER_PORT = 1236
+        self.setWindow()
         
         socket_instance = socket.socket()
-        socket_instance.connect((SERVER_ADDRESS, SERVER_PORT))
+        if self.server_address and self.server_port:
+            try:
+                socket_instance.connect((self.server_address, self.server_port))
+            except:
+                messagebox.showinfo('Programm', 'Write correct server address')
+                pygame.quit()
+                sys.exit()
         
-        threading.Thread(target=self.handle_messages, args=[socket_instance]).start()
-        
-        game_over = False
-        last_choise = False
-
-        speed = 0.1
-
-
-        pygame.init()
-        surface = pygame.display.set_mode((625, 625))
-        surface.fill(self.blue)
-        pygame.display.set_caption("Snake game")
-
-        
-        apple_cors = self.appleGrownUp(surface)
-
-        while not game_over:
-
-            while last_choise:
-                self.game_over(surface)
-                for event in pygame.event.get():
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_1:
-                            self.snake._head = (12, 12)
-                            self.snake._direction = 0
-                            self.snake._cor = [self.snake._cor[0]]
-                            pygame.display.flip()
-                            self.main()
-                        elif event.key == pygame.K_0:
-                            pygame.quit()
-                            sys.exit()
-            #time.sleep(0.1)
-            time.sleep(speed)
+            threading.Thread(target=self.handle_messages, args=[socket_instance]).start()
             
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+            game_over = False
+            last_choise = False
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        if self.snake._direction == 1:
-                            continue
-                        print('LEFT')
-                        socket_instance.send('LEFT'.encode())
-                    elif event.key == pygame.K_RIGHT:
-                        if self.snake._direction == 3:
-                            continue
-                        print('RIGHT')
-                        socket_instance.send('RIGHT'.encode())
-                    elif event.key == pygame.K_UP:
-                        if self.snake._direction == 2:
-                            continue
-                        print('UP')
-                        socket_instance.send('UP'.encode())
-                    elif event.key == pygame.K_DOWN:
-                        if self.snake._direction == 0:
-                            continue
-                        print('DOWN')
-                        socket_instance.send('DOWN'.encode())
+            speed = 0.1
 
-            self.snakeCutter(surface)
-            self.snake.move()
-            self.drawSnake(surface)
 
-            if not self.limitCheck(self.snake._head) or self.snakeDibilizmCheck(self.snake._head):
-                last_choise = True
+            pygame.init()
+            surface = pygame.display.set_mode((625, 625))
+            surface.fill(self.blue)
+            pygame.display.set_caption("Snake game")
 
-            while not apple_cors:
-                apple_cors = self.appleGrownUp(surface)
+            
+            apple_cors = self.appleGrownUp(surface)
 
-            if self.snake._head == apple_cors:
-                speed = speed - speed/10
-                pygame.draw.rect(surface, self.red, [apple_cors[0]  * 25, apple_cors[1] * 25, 25, 25])
-                apple_cors = self.appleGrownUp(surface)
-                self.snake.snakeMaker()
+            while not game_over:
+
+                while last_choise:
+                    self.game_over(surface)
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_1:
+                                self.snake._head = (12, 12)
+                                self.snake._direction = 0
+                                self.snake._cor = [self.snake._cor[0]]
+                                pygame.display.flip()
+                                self.main()
+                            elif event.key == pygame.K_0:
+                                pygame.quit()
+                                sys.exit()
+                time.sleep(0.1)
+                #time.sleep(speed)
                 
-            pygame.display.flip()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                            if self.snake._direction == 1:
+                                continue
+                            print('LEFT')
+                            socket_instance.send('LEFT'.encode())
+                        elif event.key == pygame.K_RIGHT:
+                            if self.snake._direction == 3:
+                                continue
+                            print('RIGHT')
+                            socket_instance.send('RIGHT'.encode())
+                        elif event.key == pygame.K_UP:
+                            if self.snake._direction == 2:
+                                continue
+                            print('UP')
+                            socket_instance.send('UP'.encode())
+                        elif event.key == pygame.K_DOWN:
+                            if self.snake._direction == 0:
+                                continue
+                            print('DOWN')
+                            socket_instance.send('DOWN'.encode())
+
+                self.snakeCutter(surface)
+                self.snake.move()
+                self.drawSnake(surface)
+
+                if not self.limitCheck(self.snake._head) or self.snakeDibilizmCheck(self.snake._head):
+                    last_choise = True
+
+                while not apple_cors:
+                    apple_cors = self.appleGrownUp(surface)
+
+                if self.snake._head == apple_cors:
+                    speed = speed - speed/10
+                    pygame.draw.rect(surface, self.red, [apple_cors[0]  * 25, apple_cors[1] * 25, 25, 25])
+                    apple_cors = self.appleGrownUp(surface)
+                    self.snake.snakeMaker()
+                    
+                pygame.display.flip()
 
 
 
 game = Board()
 game.main()
-
 
